88f08b08f57f7b0c3d764fdd2a774b2ea3cdea64
Christopher Agocs <chris.agocs@gmail.com>
2013-09-28 16:33:41 -0500

    adding Rails site

---
+++ yCranky.web/yCranky/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+YCranky::Application.config.secret_key_base = 'e1e1dcc9a1580051c3f007162d17e5900b2420113fd0b155024d7c61698e35270b527bd37d5dcf8d011d8f55acbe25b62f01cd167ccf0c243032c01b4f004e02'
---
+++ yCranky.web/yCranky/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+YCranky::Application.config.session_store :cookie_store, key: '_yCranky_session'
